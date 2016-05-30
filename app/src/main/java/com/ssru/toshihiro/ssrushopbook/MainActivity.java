3bf14887b9196afa1440c4a49a847462174ff8a9
Toshihiro <s56122201044@ssru.ac.th>
2016-05-30 11:58:27 +0700

    Inten (เปลี่ยนหน้าแล้ว)

--- app/src/main/java/com/ssru/toshihiro/ssrushopbook/MainActivity.java
+++ app/src/main/java/com/ssru/toshihiro/ssrushopbook/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ssru.toshihiro.ssrushopbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
+
+
+    }   //  Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
-}
+}   // Main Class
